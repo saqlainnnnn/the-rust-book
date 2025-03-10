03e7ed8e47bf20823e229d474c0ecbba83aa1719
saqlain <saqlainsibtain123@gmail.com>
2025-03-10 16:48:47 +0530

    til-structs

---
+++ projects/ch5/match_4_2/target/debug/deps/match_4_2.d
@@ -0,0 +1,5 @@
+E:\code\rust-book\projects\ch5\match_4_2\target\debug\deps\match_4_2.exe: src\main.rs
+
+E:\code\rust-book\projects\ch5\match_4_2\target\debug\deps\match_4_2.d: src\main.rs
+
+src\main.rs:
---
+++ projects/ch5/match_4_2/target/debug/match_4_2.d
@@ -0,0 +1,1 @@
+E:\code\rust-book\projects\ch5\match_4_2\target\debug\match_4_2.exe: E:\code\rust-book\projects\ch5\match_4_2\src\main.rs
