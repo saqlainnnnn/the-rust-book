7f5877a40acf11430a9051113904b7d9f67a4b6d
saqlain <saqlainsibtain123@gmail.com>
2025-03-24 03:24:10 +0530

    testing part 1

---
+++ projects/ch11/adder/target/debug/deps/adder-444c4bab68ea65f1.d
@@ -0,0 +1,5 @@
+E:\code\rust-book\projects\ch11\adder\target\debug\deps\adder-444c4bab68ea65f1.exe: src\lib.rs
+
+E:\code\rust-book\projects\ch11\adder\target\debug\deps\adder-444c4bab68ea65f1.d: src\lib.rs
+
+src\lib.rs:
---
+++ projects/ch11/adder/target/debug/deps/adder-a20978365377c4a6.d
@@ -0,0 +1,7 @@
+E:\code\rust-book\projects\ch11\adder\target\debug\deps\libadder-a20978365377c4a6.rmeta: src\lib.rs
+
+E:\code\rust-book\projects\ch11\adder\target\debug\deps\libadder-a20978365377c4a6.rlib: src\lib.rs
+
+E:\code\rust-book\projects\ch11\adder\target\debug\deps\adder-a20978365377c4a6.d: src\lib.rs
+
+src\lib.rs:
