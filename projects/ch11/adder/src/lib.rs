f1a158a14d4dad0424871a8209517fce09fe6d96
saqlain <saqlainsibtain123@gmail.com>
2025-03-24 03:30:53 +0530

    remaining test ch11

--- projects/ch11/adder/src/lib.rs
+++ projects/ch11/adder/src/lib.rs
@@ -1,13 +1,5 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) ->bool {
-        self.width > other.width && self.height > other.height
-    }
+pub fn greeting(name: &str) -> String {
+    String::from("hello!")
 }
 
 
@@ -16,16 +8,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn larger_can_hold_smaller() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle{
-            width:5,
-            height:1, 
-        };
-        
-        assert!(larger.can_hold(&smaller));
+    fn greeting_contains_name() {
+        let result = greeting("sqln");
+        assert!(result.contains("sqln"));
     }
 }
